862500935611a0c3c04ee6e833604280371c6fb0
vusvolodymyr <vusvolodimir@gmail.com>
2021-11-27 16:43:57 +0200

    lesson file of python. Wile.

---
+++ 3_while.py
@@ -0,0 +1,8 @@
+a=int(input("Please, enter final number"))
+b=int(input("Please, entering starting number"))
+c=int(input("Please, entering step of number"))
+i=0
+while b<a:
+    print("comparative results: ", b, "less then ", a)
+    b = b + c
+    i = i + 1
