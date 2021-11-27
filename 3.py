088612f128fa3643fbcb66beef718cb3d635cc6e
vusvolodymyr <vusvolodimir@gmail.com>
2021-11-27 16:40:23 +0200

    leson of python file

---
+++ 3.py
@@ -0,0 +1,9 @@
+a = int(input("enter  a"))
+b = int(input("enter b"))
+if a==b:
+    print("numbers are equal. WOW!")
+else:
+    if a>b:
+        print("first number is more then second. :) ")
+    else:
+        print("second number is more then first. :(")
